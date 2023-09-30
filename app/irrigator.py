2a224cfc0c5690831338f36adb6e94ff441ec327
Kent Hua <pitchdarkice@gmail.com>
2023-09-30 17:19:42 +0000

    add refactored weather

--- app/irrigator.py
+++ app/irrigator.py
@@ -2,9 +2,7 @@ import gpiozero
 import time
 import schedule
 import datetime
-import pyowm
-from pyowm.utils import timestamps, formatting
-import os
+from weather import *
 
 #RELAY_PIN = "BOARD16"
 RELAY_PIN = 23
@@ -15,11 +13,7 @@ duration = 45
 dt_format = "%Y-%m-%d %H:%M:%S"
 filePath = "/tmp/irrigate.out"
 
-# weather
-owm = pyowm.OWM(os.environ["OWM_API_KEY"])
-weather_mgr = owm.weather_manager()
-lat = float(os.environ["LOCATION_LAT"])
-lon = float(os.environ["LOCATION_LON"])
+weather = Weather()
 
 # Triggered by the output pin going high: active_hgh=False
 # Initially off: initial_value=False
@@ -35,12 +29,8 @@ def convertPrecipitation(value):
 
 # turn the relay on and off
 def irrigate():
-    weather = weather_mgr.weather_at_coords(lat, lon).weather
-    yesterday_epoch = formatting.to_UNIXtime(timestamps.yesterday())
-    weatherYesterday = weather_mgr.one_call_history(lat=lat, lon=lon, dt=yesterday_epoch).current
-
-    precipitation = convertPrecipitation(weather.precipitation_probability)
-    yesterdayPrecipitation = convertPrecipitation(weatherYesterday.precipitation_probability)
+    precipitation = weather.precipitation()
+    yesterdayPrecipitation = weather.yesterdayPrecipitation()
     print("T: " + str(precipitation), flush=True)
     print("Y: " + str(yesterdayPrecipitation), flush=True)
 
