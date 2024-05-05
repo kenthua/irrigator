1d56c5beceb6db7e52b424db1ba4e688047f35ec
Kent Hua <8052337+kenthua@users.noreply.github.com>
2024-05-05 10:58:49 -0700

    update the pin layout and init value

--- micropython/main.py
+++ micropython/main.py
@@ -3,6 +3,7 @@
 import sys
 sys.path.reverse()
 
+import config
 import uasyncio as asyncio
 from sched.sched import schedule, Sequence
 from time import localtime
@@ -12,23 +13,23 @@ from umqtt.simple2 import MQTTClient
 import ubinascii
 import machine
 
-p2 = Pin(2,Pin.OUT)
-mqtt_server = "192.168.4.11"
+relay = Pin(13,Pin.OUT)
+mqtt_server = config.MQTT_SERVER
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_pub = b'esp32'
 
 def irrigate():  # Demonstrate callback
-    print("on")
     try:
-        p2.on()
+        print("value: 0")
+        relay.value(0)
         client = MQTTClient(client_id, mqtt_server)
         client.connect()
-        message = "Irrigator: " + str(time.localtime())
+        message = "Irrigator: [" + str(relay.value()) + "]: " + str(time.localtime())
         client.publish(topic_pub, message, retain=True)
         client.disconnect()    
-        time.sleep_ms(500)
-        print("off")
-        p2.off()
+        time.sleep_ms(config.DURATION)
+        print("value: 1")
+        relay.value(1)
     except OSError as e:
         print("Failed")
         time.sleep(2)
@@ -36,11 +37,14 @@ def irrigate():  # Demonstrate callback
 
 async def main():
     print("Begin scheduling...")
+    print("Set relay to 1, to turn off")
+    relay.value(1)
     seq = Sequence()
     print("Current time：%s" %str(time.localtime()))
     # +7 from GMT
     # wday 0-6 (mon-sun)
-    asyncio.create_task(schedule(seq, 'irrigate', wday=5, hrs=22, mins=48))
+    asyncio.create_task(schedule(seq, 'irrigate', wday=1, hrs=14, mins=00))
+    asyncio.create_task(schedule(seq, 'irrigate', wday=3, hrs=14, mins=00))
     print("Scheduled")
     async for args in seq:
         irrigate()
