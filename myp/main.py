cdf7968299dda326fb3770f10add0560c5dc7adb
Kent Hua <pitchdarkice@gmail.com>
2024-05-07 19:49:08 -0700

    fix message

--- myp/main.py
+++ myp/main.py
@@ -3,6 +3,7 @@
 import sys
 sys.path.reverse()
 
+import json
 import config
 import uasyncio as asyncio
 from sched.sched import schedule, Sequence
@@ -16,7 +17,8 @@ import machine
 relay = Pin(13,Pin.OUT)
 mqtt_server = config.MQTT_SERVER
 client_id = ubinascii.hexlify(machine.unique_id())
-topic_pub = b'esp32'
+topic_pub = bytes(config.MQTT_TOPIC, "utf-8")
+json_message = {}
 
 def irrigate():  # Demonstrate callback
     try:
@@ -24,8 +26,11 @@ def irrigate():  # Demonstrate callback
         relay.value(0)
         client = MQTTClient(client_id, mqtt_server)
         client.connect()
-        message = "Irrigator: [" + str(relay.value()) + "]: " + str(time.localtime())
-        client.publish(topic_pub, message, retain=True)
+        json_message.update({"type": "irrigator"})
+        json_message.update({"relay_value": str(relay.value())})
+        json_message.update({"timestamp": str(time.localtime())})
+        #message = "Irrigator: [" + str(relay.value()) + "]: " + str(time.localtime())
+        client.publish(topic_pub, json.dumps(json_message), retain=True)
         client.disconnect()    
         time.sleep_ms(config.DURATION)
         print("value: 1")
@@ -39,12 +44,17 @@ async def main():
     print("Begin scheduling...")
     print("Set relay to 1, to turn off")
     relay.value(1)
+    print("Set machine freq")
+    machine.freq(config.MACHINE_FREQ)
+    print("Machine freq: " + str(machine.freq()))
     seq = Sequence()
     print("Current time：%s" %str(time.localtime()))
     # +7 from GMT
     # wday 0-6 (mon-sun)
     asyncio.create_task(schedule(seq, 'irrigate', wday=1, hrs=14, mins=00))
     asyncio.create_task(schedule(seq, 'irrigate', wday=3, hrs=14, mins=00))
+    # test
+    #asyncio.create_task(schedule(seq, 'irrigate', wday=2, hrs=02, mins=09))
     print("Scheduled")
     async for args in seq:
         irrigate()
@@ -53,3 +63,4 @@ try:
     asyncio.run(main())
 finally:
     asyncio.new_event_loop()
+
