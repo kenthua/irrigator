c728976d47f3073a4ccd87288295d67f46dc5d2d
Kent Hua <pitchdarkice@gmail.com>
2024-05-18 15:57:07 -0700

    change relay type and pin, reverse order

--- mpy/main.py
+++ mpy/main.py
@@ -14,7 +14,7 @@ from umqtt.simple2 import MQTTClient
 import ubinascii
 import machine
 
-relay = Pin(13,Pin.OUT)
+relay = Pin(1,Pin.OUT)
 mqtt_server = config.MQTT_SERVER
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_pub = bytes(config.MQTT_TOPIC, "utf-8")
@@ -23,16 +23,16 @@ json_message = {}
 def irrigate():  # Demonstrate callback
     client = MQTTClient(client_id, mqtt_server)
     try:
-        print("value: 0")
-        relay.value(0)
+        print("value: 1")
+        relay.value(1)
         client.connect()
         json_message.update({"type": "irrigator"})
         json_message.update({"relay_value": str(relay.value())})
         json_message.update({"timestamp": str(time.localtime())})
         client.publish(topic_pub, json.dumps(json_message), retain=True)
         time.sleep_ms(config.DURATION)
-        print("value: 1")
-        relay.value(1)
+        print("value: 0")
+        relay.value(0)
         json_message.update({"relay_value": str(relay.value())})
         json_message.update({"timestamp": str(time.localtime())})
         client.publish(topic_pub, json.dumps(json_message), retain=True)
@@ -45,8 +45,8 @@ def irrigate():  # Demonstrate callback
 
 async def main():
     print("Begin scheduling...")
-    print("Set relay to 1, to turn off")
-    relay.value(1)
+    print("Set relay to 0, to turn off")
+    relay.value(0)
     print("Set machine freq")
     machine.freq(config.MACHINE_FREQ)
     print("Machine freq: " + str(machine.freq()))
@@ -57,7 +57,7 @@ async def main():
     asyncio.create_task(schedule(seq, 'irrigate', wday=1, hrs=14, mins=00))
     asyncio.create_task(schedule(seq, 'irrigate', wday=3, hrs=14, mins=00))
     # test
-    #asyncio.create_task(schedule(seq, 'irrigate', wday=4, hrs=02, mins=44))
+    #asyncio.create_task(schedule(seq, 'irrigate', wday=5, hrs=22, mins=33))
     print("Scheduled")
     async for args in seq:
         irrigate()
@@ -66,5 +66,4 @@ try:
     asyncio.run(main())
 finally:
     asyncio.new_event_loop()
-
 
