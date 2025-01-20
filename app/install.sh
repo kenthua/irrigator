a08f84a276c312530bd10eee9087d36ed6195416
Kent Hua <pitchdarkice@gmail.com>
2025-01-20 08:10:42 +0000

    add files to venus os

---
+++ app/install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVICE_NAME=$(basename $SCRIPT_DIR)
+
+echo
+echo "Installing $SERVICE_NAME..."
+
+# set permissions for script files
+echo "Setting permissions..."
+chmod 755 $SCRIPT_DIR/*.py
+chmod 755 $SCRIPT_DIR/*.sh
+chmod 755 $SCRIPT_DIR/service/run
+chmod 755 $SCRIPT_DIR/service/log/run
+
+# check dependencies
+python -c "import gpiozero"
+if [ $? -gt 0 ]
+then
+    echo "Installing requirements..."
+    python -m pip install -r requirements.txt
+    if [ $? -gt 0 ]
+    then
+        # if pip command fails install pip and then try again
+        opkg update && opkg install python3-pip
+        python -m pip install -r requirements.txt
+    fi
+fi
+
+# create sym-link to run script in deamon
+if [ ! -L /service/$SERVICE_NAME ]; then
+    echo "Creating service..."
+    ln -s $SCRIPT_DIR/service /service/$SERVICE_NAME
+else
+    echo "Service already exists."
+fi
+
+# add install-script to rc.local to be ready for firmware update
+filename=/data/rc.local
+if [ ! -f $filename ]
+then
+    touch $filename
+    chmod 755 $filename
+    echo "#!/bin/bash" >> $filename
+    echo >> $filename
+fi
+
+# if not alreay added, then add to rc.local
+grep -qxF "bash $SCRIPT_DIR/install.sh" $filename || echo "bash $SCRIPT_DIR/install.sh" >> $filename
+
+echo
